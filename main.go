e6d236b54b5ba21459e7b9283d62d4cc245b2d59
tapadar13 <monsurismyname@gmail.com>
2024-11-04 20:11:05 +0530

    Initial commit: Basic Fiber server setup with Air configuration

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{
+			"message": "This is a JSON response",
+		})
+	})
+
+	log.Fatal(app.Listen(":4000"))
+}
